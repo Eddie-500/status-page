7bfe67cfaf1b8505ed72da69cfaed11dc5098039
Eddie-500 <eddiec_5@protonmail.com>
2025-09-25 01:14:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ async function loadData() {
   const res = await fetch("data.json");
   const data = await res.json();
 
-  // Status
-  document.getElementById("lastUpdate").textContent =
-    "Last update: " + data.status.lastUpdate;
-
-  const statusSection = document.getElementById("status");
-  statusSection.innerHTML = `
-    <div class="status-box ${data.status.overall === "operational" ? "operational" : "down"}">
-      <strong>${data.status.message}</strong>
-    </div>
-    <h2>Services</h2>
-    ${data.status.services.map(s =>
-      `<div class="status-box ${s.status}">
-        ${s.name} - ${s.uptime} - ${s.status}
-      </div>`).join("")}
-  `;
+// Status general
+statusSection.innerHTML = `
+  <div class="status-box">
+    <strong>
+      ${data.status.state === "operational" ? "✅ <span class='status-online'>All systems operational</span>" : ""}
+      ${data.status.state === "updating" ? "<span class='loading'></span> 🟠 <span class='status-updating'>Updating</span>" : ""}
+      ${data.status.state === "down" ? "❌ <span class='status-down'>Service offline</span>" : ""}
+    </strong>
+  </div>
+  <h2>Services</h2>
+  ${data.status.services.map(s => `
+    <div class="status-box">
+      ${s.name} - ${s.uptime} - 
+      ${s.status === "operational" ? "✅ <span class='status-online'>Online</span>" : ""}
+      ${s.status === "updating" ? "<span class='loading'></span> 🟠 <span class='status-updating'>Updating</span>" : ""}
+      ${s.status === "down" ? "❌ <span class='status-down'>Offline</span>" : ""}
+    </div>`).join("")}
+`;
 
   // Maintenance
   const maintSection = document.getElementById("maintenance");
